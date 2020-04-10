55a64180a4b05eca5503580f12b6246d2130b8b5
Randolph Gordon <randolph.gordon@poplogics.com>
2020-04-10 16:00:37 +0200

    added server and extras

--- backend/server.js
+++ backend/server.js
@@ -6,26 +6,26 @@ const app = express();
 app.use(express.json());
 
 // mounting a router to the app
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
 });
 
-app.use("/", function(req, res) {
-    fetch(`https://wakatime.com/api/v1/users/poplogics/goals`, {
-        headers: {
-            Authorization:
-                "Bearer sec_dRB2WSJnqXefOE7PrC8DGzVkEvlLgP4qHVjTzzgLQehTSvqU3svwPi6vRIvG7K3FuC01dKhyYfWlwbeS",
-        },
+app.use("/", function (req, res) {
+  console.log("fetch operation");
+  fetch(`https://wakatime.com/api/v1/users/poplogics/stats/last_7_days`, {
+    headers: {
+      Authorization:
+        "Bearer sec_1xeh91EskwoCkwaZlPr04MyFqGuqevVRewJgQGGu0HoUZVUuxEMa6eV6SYCdmmtZ31UQgjKztjLKJrpS",
+    },
+  })
+    .then((response) => {
+      return response.json();
     })
-        .then((response) => {
-            console.log(response);
-            return response.json();
-        })
-        .then((data) => {
-            return res.json(data);
-        });
+    .then((data) => {
+      return res.json(data);
+    });
 });
 
 app.listen(3001, () => console.log("listening from port 3001"));
--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
-import React,{useEffect} from 'react';
-import './App.scss';
+import React, { useEffect, useState } from "react";
+import "./App.scss";
 
 function App() {
-    useEffect(() =>{
-        
-    } )
+  const [error, setError] = useState(false);
+  const [stats, setStats] = useState([]);
+
+  /**
+   *
+   * @returns {Promise<void>}
+   */
+  async function fetchData() {
+    console.log("fetching data..");
+    const url = "http://localhost:3001";
+    const response = await fetch(url);
+    response
+      .json()
+      .then((data) => {
+        console.log(data, typeof data);
+        setStats(Object.values(data));
+      })
+      .catch((error) => setError(error));
+  }
+
+  useEffect(() => {
+    console.log("useEffect");
+    fetchData();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
+        {!error && (
+          <div>
+            {Object.values(stats).map((item, index) => (
+              <p key={index}>
+                {index}
+                {}
+              </p>
+            ))}
+          </div>
+        )}
       </header>
     </div>
   );
