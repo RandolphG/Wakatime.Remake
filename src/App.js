b4bae30dc6b97fb3f0b55a9dc58a898e6b386a55
randolphg <poplogics@MSI.localdomain>
2020-04-10 19:46:30 +0200

    local changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React,{useEffect} from 'react';
-import './App.scss';
+import React, { useEffect } from "react";
+import "./App.scss";
 
 function App() {
-    useEffect(() =>{
-        
-    } )
+  useEffect(() => {
+    fetch("http://localhost:3000").then((response) =>
+      response.json().then((data) => {
+        console.log(data);
+        return data;
+      })
+    );
+  });
   return (
     <div className="App">
       <header className="App-header">
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-
       </header>
     </div>
   );
